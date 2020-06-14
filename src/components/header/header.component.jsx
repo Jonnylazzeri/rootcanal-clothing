ad90cd3267883139ac57ef5b593250c18dc4604b
Jonathan Lazzeri <jonnylazzeri94@gmail.com>
2020-06-14 22:15:40 +0300

    added a responsive dropdown window from the shopping bag icon

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './header.styles.scss';
 import { Link } from 'react-router-dom';
+import CartIcon from '../cart-icon/cart-icon.component'
 import { connect } from 'react-redux';
 import { auth } from '../../firebase/firebase.utils';
 import { ReactComponent as Logo} from '../../assets/crown.svg';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component'
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser, hidden }) => (
   <div className='header'>
     <Link className='logo-container' to="/">
       <Logo className = 'logo'/>
@@ -18,12 +20,19 @@ const Header = ({ currentUser }) => (
         :
         <Link className='option' to='/signin'>SIGN IN</Link>
       }
+      <CartIcon />
     </div>
+    {
+      hidden ? null :
+      <CartDropdown />
+
+    }
   </div>
 );
 
-const mapStateToProps = state => ({
-  currentUser: state.user.currentUser
+const mapStateToProps = ({user: { currentUser }, cart: { hidden }}) => ({
+  currentUser,
+  hidden
 })
 
 export default connect(mapStateToProps)(Header);
